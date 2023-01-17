e23a594fff57b48c3dc3a02a0586da32627d97d1
Teerawat <meguminkonosuba7@gmail.com>
2023-01-17 19:52:35 +0700

    999

--- app/src/main/java/com/example/travelinpech/MainActivity.java
+++ app/src/main/java/com/example/travelinpech/MainActivity.java
@@ -7,14 +7,57 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DataBase db;
+    private EditText editTextNewCategory;
+    private Button buttonCategory;
+    private Spinner spinnerCategories;
+    private List<String> listCategories = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // connect database
+        db = new DataBase(this);
+        if(db == null)
+            db = new DataBase(this);
+        editTextNewCategory = (EditText) findViewById(R.id.edtName);
+        spinnerCategories = (Spinner) findViewById(R.id.spinner_category);
+        buttonCategory = (Button) findViewById(R.id.btnOK);
+        buttonCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newCategory = editTextNewCategory.getText().toString();
+                if(newCategory.equalsIgnoreCase("")){
+                    Toast.makeText(getApplicationContext(), "Please Enter new Category", Toast.LENGTH_SHORT).show();
+                }else {
+                    db.addCategory(new Category(newCategory ));
+                    prepareData();
+                    editTextNewCategory.setText("");
+                    Toast.makeText(MainActivity.this, "New Category was Successfully added to Database", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        prepareData();
+
     }
+
+    public void prepareData(){
+        listCategories = db.getAllCategories();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, android.R.id.text1, listCategories);
+        spinnerCategories.setAdapter(adapter);
+    }
+
     public void MainPage(View view){
         Button btnOK = (Button) findViewById(R.id.btnOK);
         Intent intent = new Intent(MainActivity.this,Main.class);
